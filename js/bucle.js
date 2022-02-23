1d4e50c5869cb2ad391a2f5ebd644895b15727e8
kevin-bermudez <kevinbermudezmejia@gmail.com>
2022-02-23 17:27:23 -0500

    feat: split game in two roads and two cars

--- js/bucle.js
+++ js/bucle.js
@@ -25,26 +25,47 @@ function endGame() {
 
 function gamePlay() {
   let car = document.querySelector(".car");
+  let car2 = document.querySelector(".car2");
+
   let road = gamearea.getBoundingClientRect();
+  let road2 = gamearea2.getBoundingClientRect();
 
   if (player.start) {
     moveLines();
     moveCar(car);
-    if (keys.ArrowUp && player.y > road.top + 70) {
-      player.y -= player.speed;
+    moveCar(car2);
+
+    if (keys.w && player.y > road.top + 70) {
+      player.y -= player.speedCar;
     }
-    if (keys.ArrowDown && player.y < road.bottom - 70) {
-      player.y += player.speed;
+    if (keys.s && player.y < road.bottom - 70) {
+      player.y += player.speedCar;
     }
-    if (keys.ArrowLeft && player.x > 0) {
-      player.x -= player.speed;
+    if (keys.a && player.x > 0) {
+      player.x -= player.speedCar;
     }
-    if (keys.ArrowRight && player.x < road.width - 50) {
-      player.x += player.speed;
+    if (keys.d && player.x < road.width - 50) {
+      player.x += player.speedCar;
+    }
+
+    if (keys.ArrowUp && player.y2 > road.top + 70) {
+      player.y2 -= player.speedCar;
+    }
+    if (keys.ArrowDown && player.y2 < road.bottom - 70) {
+      player.y2 += player.speedCar;
+    }
+    if (keys.ArrowLeft && player.x2 > 0) {
+      player.x2 -= player.speedCar;
+    }
+    if (keys.ArrowRight && player.x2 < road.width - 50) {
+      player.x2 += player.speedCar;
     }
 
     car.style.top = player.y + "px";
     car.style.left = player.x + "px";
+
+    car2.style.top = player.y2 + "px";
+    car2.style.left = player.x2 + "px";
 
     window.requestAnimationFrame(gamePlay);
     //console.log(player.score++);
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const score = document.querySelector(".Score");
 const startscreen = document.querySelector(".StartScreen");
 const gamearea = document.querySelector(".GameArea");
+const gamearea2 = document.querySelector(".GameArea2");
 
-let player = { speed: 5, score: 0 };
+let player = { speed: 1, score: 0, speedCar: 5 };
 let highest = 0;
 startscreen.addEventListener("click", start);
 
@@ -11,6 +12,10 @@ let keys = {
   ArrowDown: false,
   ArrowRight: false,
   ArrowLeft: false,
+  w: false,
+  s: false,
+  d: false,
+  a: false,
 };
 
 document.addEventListener("keydown", keyDown);
@@ -23,6 +28,7 @@ function Reset() {
 function start() {
   startscreen.classList.add("hide");
   gamearea.innerHTML = "";
+  gamearea2.innerHTML = "";
 
   player.start = true;
   player.score = 0;
@@ -34,14 +40,28 @@ function start() {
     roadline.y = x * 150;
     roadline.style.top = roadline.y + "px";
     gamearea.appendChild(roadline);
+
+    let roadline2 = document.createElement("div");
+    roadline2.setAttribute("class", "lines2");
+    roadline2.y = x * 150;
+    roadline2.style.top = roadline.y + "px";
+    gamearea.appendChild(roadline);
+    gamearea2.appendChild(roadline2);
   }
 
   let car = document.createElement("div");
   car.setAttribute("class", "car");
   gamearea.appendChild(car);
 
+  let car2 = document.createElement("div");
+  car2.setAttribute("class", "car2");
+  gamearea2.appendChild(car2);
+
   player.x = car.offsetLeft;
   player.y = car.offsetTop;
+
+  player.x2 = car2.offsetLeft;
+  player.y2 = car2.offsetTop;
 
   for (x = 0; x < 3; x++) {
     let othercar = document.createElement("div");
@@ -49,6 +69,15 @@ function start() {
     othercar.y = (x + 1) * 350 * -1;
     othercar.style.top = othercar.y + "px";
     othercar.style.left = Math.floor(Math.random() * 350) + "px";
+
     gamearea.appendChild(othercar);
+
+    let othercar2 = document.createElement("div");
+    othercar2.setAttribute("class", "other");
+    othercar2.y = (x + 1) * 350 * -1;
+    othercar2.style.top = othercar.y + "px";
+    othercar2.style.left = Math.floor(Math.random() * 350) + "px";
+
+    gamearea2.appendChild(othercar2);
   }
 }
